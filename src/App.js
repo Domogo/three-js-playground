017dcee3d097ee9fbd94439fe4a51bae2230582e
Domogo <domsatrapa@gmail.com>
2023-01-10 02:01:41 +0100

    files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Chest } from "./components/chest";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas id="test">
+      <pointLight position={[2, 8, 20]} />
+      <ambientLight />
+      <Chest position={[0, -1, 1]} />
+    </Canvas>
   );
 }
 
---
+++ src/components/chest.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { a, useSpring } from "@react-spring/three"
+
+
+export function Chest(props) {
+  const ref = useRef();
+  const topRef = useRef();
+  const [topRaised, setTopRaised] = useState(false);
+  const { position } = useSpring({ position: topRaised ? [0, 2.17, -0.57] : [0, 1.17, -0.57] });
+
+  const { nodes, materials } = useGLTF("models/chest.gltf");
+  useFrame((state, delta) => {ref.current.rotation.y += 0.01});
+
+  const moveTop = () => {
+    setTopRaised(!topRaised);
+  }
+
+  console.log(position);
+  return (
+    <group rotation-x={0.21} ref={ref} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ChestBase071.geometry}
+        material={materials.Chest}
+        position={[0, 0.21, 0]}
+        onClick={moveTop}
+
+      />
+      <a.mesh
+        ref={topRef}
+        castShadow
+        receiveShadow
+        geometry={nodes.ChestTop071.geometry}
+        material={materials.Chest}
+        position={position}
+        onClick={moveTop}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/chest.gltf");
